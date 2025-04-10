2246b0719fe2cfcf34e98acad63d9a586cf528be
ronny-souza <ronnymarinho2@gmail.com>
2025-04-09 21:53:10 -0300

    ✨ feat: adicionando configuração do vault e do RabbitMQ

--- src/main/java/br/com/uboard/UboardApplication.java
+++ src/main/java/br/com/uboard/UboardApplication.java
@@ -2,12 +2,14 @@ package br.com.uboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class UboardApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UboardApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UboardApplication.class, args);
+    }
 
 }
