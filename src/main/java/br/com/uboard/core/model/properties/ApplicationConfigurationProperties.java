da026c020cbf1ebcf1270edfb450a35a5e159558
ronny-souza <ronnymarinho2@gmail.com>
2025-04-09 22:03:28 -0300

    ✨ feat: classe de propriedades da aplicação

---
+++ src/main/java/br/com/uboard/core/model/properties/ApplicationConfigurationProperties.java
@@ -0,0 +1,23 @@
+package br.com.uboard.core.model.properties;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+@ConfigurationProperties("app.configuration")
+public class ApplicationConfigurationProperties {
+
+    @Value("#{'${app.configuration.allowed-origins}'.split(',')}")
+    private List<String> allowedOrigins;
+
+    public List<String> getAllowedOrigins() {
+        return allowedOrigins;
+    }
+
+    public void setAllowedOrigins(List<String> allowedOrigins) {
+        this.allowedOrigins = allowedOrigins;
+    }
+}
