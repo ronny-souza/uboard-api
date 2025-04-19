2b89823928b69e4ef940fd57c962b992e6d6ce32
ronny-souza <ronnymarinho2@gmail.com>
2025-04-19 15:36:24 -0300

    ✨ feat: serviço para recuperação de progresso e status de uma tarefa para monitoramento

--- src/main/java/br/com/uboard/core/repository/TaskRepository.java
+++ src/main/java/br/com/uboard/core/repository/TaskRepository.java
@@ -1,11 +1,18 @@
 package br.com.uboard.core.repository;
 
 import br.com.uboard.core.model.Task;
+import br.com.uboard.core.model.transport.TaskProgressDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
-public interface TaskRepository extends JpaRepository<Task, Long> {
+public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificationExecutor<Task> {
 
     Optional<Task> findByUuid(String uuid);
+
+    @Query("SELECT t.status AS status, t.progress as progress FROM Task t WHERE t.uuid = :uuid")
+    Optional<TaskProgressDTO> getTaskProgress(@Param("uuid") String uuid);
 }
