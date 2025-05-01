f68068d0d2fbc149abcb2c8250c58241ba1509d4
ronny-souza <ronnymarinho2@gmail.com>
2025-04-30 22:49:57 -0300

    ✨ feat: adição de listagem de grupos e projetos da credencial

--- src/main/java/br/com/uboard/core/controller/CredentialController.java
+++ src/main/java/br/com/uboard/core/controller/CredentialController.java
@@ -1,6 +1,8 @@
 package br.com.uboard.core.controller;
 
 import br.com.uboard.core.model.enums.TaskOperationEnum;
+import br.com.uboard.core.model.external.GitGroupInterface;
+import br.com.uboard.core.model.external.GitProjectInterface;
 import br.com.uboard.core.model.filters.CredentialFiltersDTO;
 import br.com.uboard.core.model.operations.CreateCredentialForm;
 import br.com.uboard.core.model.operations.CreateTaskForm;
@@ -9,7 +11,10 @@ import br.com.uboard.core.model.transport.SessionUserDTO;
 import br.com.uboard.core.model.transport.TaskDTO;
 import br.com.uboard.core.service.CreateTaskService;
 import br.com.uboard.core.service.ListCredentialsService;
+import br.com.uboard.core.service.ListGitGroupsService;
+import br.com.uboard.core.service.ListGitProjectsService;
 import br.com.uboard.exception.CreateTaskException;
+import br.com.uboard.exception.UboardApplicationException;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -21,17 +26,24 @@ import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/credentials")
 public class CredentialController {
-
     private final CreateTaskService createTaskService;
     private final ListCredentialsService listCredentialsService;
+    private final ListGitProjectsService listGitProjectsService;
+    private final ListGitGroupsService listGitGroupsService;
 
     public CredentialController(CreateTaskService createTaskService,
-                                ListCredentialsService listCredentialsService) {
+                                ListCredentialsService listCredentialsService,
+                                ListGitProjectsService listGitProjectsService,
+                                ListGitGroupsService listGitGroupsService) {
         this.createTaskService = createTaskService;
         this.listCredentialsService = listCredentialsService;
+        this.listGitProjectsService = listGitProjectsService;
+        this.listGitGroupsService = listGitGroupsService;
     }
 
     @GetMapping
@@ -43,6 +55,25 @@ public class CredentialController {
                 pageable, sessionUserDTO, filters
         );
         return ResponseEntity.ok(credentialsPage);
+    }
+
+    @GetMapping("/{id}/projects")
+    public ResponseEntity<List<GitProjectInterface>> listProjects(@AuthenticationPrincipal Jwt jwt,
+                                                                  @PathVariable String id,
+                                                                  @RequestParam(required = false, name = "page_size", defaultValue = "500") String pageSize)
+            throws UboardApplicationException {
+        SessionUserDTO sessionUserDTO = new SessionUserDTO(jwt);
+        return ResponseEntity.ok(this.listGitProjectsService.listGitProjects(id, sessionUserDTO, pageSize));
+    }
+
+    @GetMapping("/{id}/groups")
+    public ResponseEntity<List<GitGroupInterface>> listGroups(@AuthenticationPrincipal Jwt jwt,
+                                                              @PathVariable String id,
+                                                              @RequestParam(required = false, name = "page_size", defaultValue = "500") String pageSize)
+            throws UboardApplicationException {
+        SessionUserDTO sessionUserDTO = new SessionUserDTO(jwt);
+        List<GitGroupInterface> response = this.listGitGroupsService.listGitGroups(id, sessionUserDTO, pageSize);
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping
@@ -59,7 +90,7 @@ public class CredentialController {
         );
 
         return ResponseEntity.created(uriComponentsBuilder
-                .path("/tasks/{id}")
+                .path("/credentials/{id}")
                 .buildAndExpand(response.uuid())
                 .toUri()
         ).body(response);
