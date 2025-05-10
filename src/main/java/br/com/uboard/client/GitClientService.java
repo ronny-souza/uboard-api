51892f2f8e5e1acde18edce546d46230eea18424
ronny-souza <ronnymarinho2@gmail.com>
2025-05-10 11:23:57 -0300

    ✨ feat: comunicação com o openfeign para chamadas de listagem de projetos, grupos, milestones e issues no Gitlab

--- src/main/java/br/com/uboard/client/GitClientService.java
+++ src/main/java/br/com/uboard/client/GitClientService.java
@@ -1,5 +1,6 @@
 package br.com.uboard.client;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.Map;
@@ -8,7 +9,35 @@ public interface GitClientService {
 
     ResponseEntity<String> getCurrentUser(Map<String, String> headers);
 
-    ResponseEntity<String> listProjects(Map<String, String> headers, Map<String, Object> params);
+    default ResponseEntity<String> listProjects(Map<String, String> headers, Map<String, Object> params) {
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).build();
+    }
 
-    ResponseEntity<String> listGroups(Map<String, String> headers, Map<String, Object> params);
+    default ResponseEntity<String> listGroups(Map<String, String> headers, Map<String, Object> params) {
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).build();
+    }
+
+    default ResponseEntity<String> listProjectMilestones(String id, Map<String, String> headers, Map<String, Object> params) {
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).build();
+    }
+
+    default ResponseEntity<String> listGroupMilestones(String id, Map<String, String> headers, Map<String, Object> params) {
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).build();
+    }
+
+    default ResponseEntity<String> listProjectMilestoneIssues(String id, Map<String, String> headers, Map<String, Object> params) {
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).build();
+    }
+
+    default ResponseEntity<String> listGroupMilestoneIssues(String id, Map<String, String> headers, Map<String, Object> params) {
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).build();
+    }
+
+    default ResponseEntity<String> getSingleProjectMilestone(String projectId, String milestoneId, Map<String, String> headers) {
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).build();
+    }
+
+    default ResponseEntity<String> getSingleGroupMilestone(String groupId, String milestoneId, Map<String, String> headers) {
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).build();
+    }
 }
--- src/main/java/br/com/uboard/client/gitlab/GitlabClientService.java
+++ src/main/java/br/com/uboard/client/gitlab/GitlabClientService.java
@@ -3,6 +3,7 @@ package br.com.uboard.client.gitlab;
 import br.com.uboard.client.GitClientService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,9 +17,47 @@ public interface GitlabClientService extends GitClientService {
 
     @Override
     @GetMapping("/projects")
-    ResponseEntity<String> listProjects(@RequestHeader Map<String, String> headers, @RequestParam Map<String, Object> params);
+    ResponseEntity<String> listProjects(@RequestHeader Map<String, String> headers,
+                                        @RequestParam Map<String, Object> params);
 
     @Override
     @GetMapping("/groups")
-    ResponseEntity<String> listGroups(@RequestHeader Map<String, String> headers, @RequestParam Map<String, Object> params);
+    ResponseEntity<String> listGroups(@RequestHeader Map<String, String> headers,
+                                      @RequestParam Map<String, Object> params);
+
+    @Override
+    @GetMapping("/projects/{projectId}/milestones/{milestoneId}")
+    ResponseEntity<String> getSingleProjectMilestone(@PathVariable String projectId,
+                                                     @PathVariable String milestoneId,
+                                                     @RequestHeader Map<String, String> headers);
+
+    @Override
+    @GetMapping("/groups/{groupId}/milestones/{milestoneId}")
+    ResponseEntity<String> getSingleGroupMilestone(@PathVariable String groupId,
+                                                   @PathVariable String milestoneId,
+                                                   @RequestHeader Map<String, String> headers);
+
+    @Override
+    @GetMapping("/projects/{id}/milestones")
+    ResponseEntity<String> listProjectMilestones(@PathVariable String id,
+                                                 @RequestHeader Map<String, String> headers,
+                                                 @RequestParam Map<String, Object> params);
+
+    @Override
+    @GetMapping("/groups/{id}/milestones")
+    ResponseEntity<String> listGroupMilestones(@PathVariable String id,
+                                               @RequestHeader Map<String, String> headers,
+                                               @RequestParam Map<String, Object> params);
+
+    @Override
+    @GetMapping("/projects/{id}/issues")
+    ResponseEntity<String> listProjectMilestoneIssues(@PathVariable String id,
+                                                      @RequestHeader Map<String, String> headers,
+                                                      @RequestParam Map<String, Object> params);
+
+    @Override
+    @GetMapping("/groups/{id}/issues")
+    ResponseEntity<String> listGroupMilestoneIssues(@PathVariable String id,
+                                                    @RequestHeader Map<String, String> headers,
+                                                    @RequestParam Map<String, Object> params);
 }
