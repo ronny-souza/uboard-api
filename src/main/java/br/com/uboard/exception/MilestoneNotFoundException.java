34c9bfd7862ce3d77c4b4ad32568b358924b1ae3
ronny-souza <ronnymarinho2@gmail.com>
2025-05-10 11:29:44 -0300

    ✨ feat: adicionando novas exceções do projeto

---
+++ src/main/java/br/com/uboard/exception/MilestoneNotFoundException.java
@@ -0,0 +1,8 @@
+package br.com.uboard.exception;
+
+public class MilestoneNotFoundException extends UboardApplicationException {
+
+    public MilestoneNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/br/com/uboard/exception/OrganizationNotFoundException.java
@@ -0,0 +1,8 @@
+package br.com.uboard.exception;
+
+public class OrganizationNotFoundException extends UboardApplicationException {
+
+    public OrganizationNotFoundException(String message) {
+        super(message);
+    }
+}
